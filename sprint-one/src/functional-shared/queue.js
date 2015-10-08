54e4d3ea2ae83e547c9f0a7e89b348c521a5f16d
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-10-08 19:24:23 -0400

    functional shared stack, passing tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -6,3 +6,20 @@ var Queue = function(){
 var queueMethods = {};
 
 
+/*
+var Car = function (loc) {
+  var obj = {loc:loc};
+  extend(obj, Car.methods);
+  return obj;
+};
+Car.methods = {
+  var move : function () { this.loc++; },
+  var off : function () { return; }
+};
+
+var amy = Car(1);
+amy.move();
+var ben = Car(9);
+ben.move();
+
+*/
