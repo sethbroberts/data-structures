e8c633ddd3665ea69755c83958be429c82545bc5
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-10-09 18:18:23 -0400

    almost done with data structures pt 1, notes on pt 2

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.front = 0;
+  this.back = 0;
+  this.storage = {};
 };
 
+Queue.prototype.size = function () {
+  return this.back - this.front;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.back++;
+};
+
+Queue.prototype.dequeue = function () {
+  if (this.size() > 0) {
+    this.front++;
+  }
+};
 
